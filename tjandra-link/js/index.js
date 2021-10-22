cd9427a12ae12d5f27a815d1d68365ede7348a2b
Tjandra Putra <57522674+Tjandra-Putra@users.noreply.github.com>
2021-10-22 19:52:33 +0800

    Add files via upload

---
+++ tjandra-link/js/index.js
@@ -0,0 +1,39 @@
+var typed = new Typed(".type", {
+  strings: ["page.", "world."],
+  typeSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
+
+// Bg Music
+var isPlaying = false;
+
+var audio = document.getElementById("myMusic");
+// audio.volume = 0.03;
+audio.volume = 0.1;
+
+// audio.play();
+
+function musicHandler() {
+  var audio = document.getElementById("myMusic");
+
+  if (isPlaying === false) {
+    isPlaying = true;
+    audio.play();
+
+    var iconPause = document.getElementById("pause");
+    var iconPlay = document.getElementById("play");
+    iconPlay.style.visibility = "visible";
+
+    iconPause.style.visibility = "hidden";
+  } else if (isPlaying === true) {
+    isPlaying = false;
+    audio.pause();
+
+    var iconPause = document.getElementById("pause");
+    var iconPlay = document.getElementById("play");
+    iconPlay.style.visibility = "hidden";
+
+    iconPause.style.visibility = "visible";
+  }
+}
